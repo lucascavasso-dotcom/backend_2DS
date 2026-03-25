bc491f4b5687373a17474bdc5112d53ab2386be8
lucascavasso-dotcom <lucas.cavasso@escola.pr.gov.br>
2026-03-25 11:36:53 +0000

    finalizamos OBJ em js

---
+++ objetos/obj-01.js
@@ -0,0 +1,13 @@
+//objetos sao estruturas chave-valor
+//tambem chamadas "key value"
+//objetos podem conter: arrays, string,
+//possuem metodos proprios
+
+const jogador = {
+    nome: "Ronaldo",
+    gols: 292,
+    time: ['palmeiras', 'coxa', 'corinthians', 'flamengo'],
+}
+console.log(jogador.nome);
+console.log(jogador.gols);
+console.log(jogador.time);
---
+++ objetos/obj-02.js
@@ -0,0 +1,22 @@
+// a partir do objeto de exemplo crie:
+//um objeto com o tema a sua escolha
+// deve possuir no minimo 6 propriedades
+// imprima de um jeito amigavel 
+// ex: console.log("cor do veiculo : " + carro.cor)
+
+const celular = {
+    marca: "Apple",
+    modelo: "iPhone 13 Pro",
+    cor: "Prata",
+    armazenamento: "256GB",
+    camera: "12MP",
+    bateria: "3095mAh"
+}
+console.log("------meu celular------");
+console.log("Marca do celular: " + celular.marca);
+console.log("Modelo do celular: " + celular.modelo);
+console.log("Cor do celular: " + celular.cor);
+console.log("Armazenamento do celular: " + celular.armazenamento);
+console.log("Camera do celular: " + celular.camera);
+console.log("Bateria do celular: " + celular.bateria);  
+console.log("-----------------------");
